753d4b73717e316277cf4ebf83c8b132fbc2ddb3
davemerritt <davidmerritt6@gmail.com>
2013-11-14 12:09:06 -0500

    Initial Commit

---
+++ connoisseur/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Connoisseur::Application.config.secret_key_base = 'b4e3231a1519939bc9ddd9bbb23bc5579b7526c29980770a79b8da72b10cab53424fac8fa01acdf3b3a1736d153809d98446e1f8c40bb067f0201f3d7b609a27'
